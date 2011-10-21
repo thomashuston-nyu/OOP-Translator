ba49d4a010badd9426ee0f59c8bd3a09af7f0044
Susana Delgadillo <blueberri.nights@gmail.com>
2011-10-21 18:05:00 -0400

    added DoWhileStatement and SwitchStatement

--- translator/DoWhileStatement.java
+++ translator/DoWhileStatement.java
@@ -1,3 +1,74 @@
 /**
  * Statement Expression
  */
+
+package translator;
+
+import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Visitor;
+
+public class DoWhileStatement extends Statement {
+ 
+  private Statement statement;
+  private Expression expression;
+
+  public DoWhileStatement(GNode n) {
+    statement = null;
+    expression = null;
+    visit(n);
+  }
+ 
+  public void visitExpression(GNode n) {
+    expression = new Expression(n);
+  }
+
+  public void visitBlock(GNode n) {
+    statement = new Block(n);
+  }
+  
+  public void visitBreakStatement(GNode n) {
+    statement = new BreakStatement(n);
+  }
+  
+  public void visitConditionalStatement(GNode n) {
+    statement = new ConditionalStatement(n);
+  }
+  
+  public void visitContinueStatement(GNode n) {
+    statement = new ContinueStatement(n);    
+  }
+  
+  public void visitDoWhileStatement(GNode n) {
+    statement = new DoWhileStatement(n);
+  }
+  
+  public void visitExpressionStatement(GNode n) {
+    statement = new ExpressionStatement(n);
+  }
+  
+  public void visitForStatement(GNode n) {
+    statement = new ForStatement(n);
+  }
+  
+  public void visitReturnStatement(GNode n) {
+    statement = new ReturnStatement(n);
+  }
+  
+  public void visitSwitchStatement(GNode n) {
+    statement = new SwitchStatement(n);
+  }
+  
+  public void visitThrowStatement(GNode n) {
+    statement = new ThrowStatement(n);
+  }
+  
+  public void visitTryCatchFinallyStatement(GNode n) {
+    statement = new TryCatchFinallyStatement(n);
+  }
+  
+  public void visitWhileStatement(GNode n) {
+    statement = new WhileStatement(n);
+  }
+  
+}
--- translator/SwitchStatement.java
+++ translator/SwitchStatement.java
@@ -1,3 +1,31 @@
 /**
  * Expression SwitchClause*
  */
+
+package translator;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Visitor;
+
+public class SwitchStatement extends Statement {
+  
+  private Expression expression;
+  private List<SwitchClause> switchClause;
+ 
+  public SwitchStatement() {
+    switchClause = new ArrayList<SwitchClause>();
+    visit(n);
+  }
+
+  public void visitExpression(GNode n) {
+    expression = new Expression(n);
+  }
+
+  public void visitSwitchClause(GNode n) {
+    switchClause.add(new SwitchClause(n));
+  }
+}
