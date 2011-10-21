4ee4ce9e311b7792b4f679a045a5a760937cbc91
Susana Delgadillo <blueberri.nights@gmail.com>
2011-10-21 19:19:57 -0400

    added ExpressionList and LogicalOrExpression

--- translator/LogicalOrExpression.java
+++ translator/LogicalOrExpression.java
@@ -2,3 +2,28 @@
  * (LogicalOrExpression LogicalAndExpression)/
  * yyValue:LogicalAndExpression
  */
+
+package translator;
+
+import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Visitor;
+
+public class LogicalOrExpression extends TranslationVisitor {
+
+  private LogicalOrExpression logicalOrExpression;
+  private LogicalAndExpression logicalAndExpression;
+
+  public LogicalOrExpression() {
+    visit(n);
+  }
+
+  public void visitLogicalOrExpression(GNode n) {
+    logicalOrExpression = new LogicalOrExpression();
+  }
+
+  public void visitLogicalAndExpression(GNode n) {
+    logicalAndExpression = new LogicalAndExpression();
+  }
+
+}
