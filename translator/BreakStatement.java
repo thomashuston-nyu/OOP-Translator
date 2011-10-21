d2a207dd4ac978c7204276c5f935c17336978b2d
marta wilgan <mmw319@nyu.edu>
2011-10-21 16:48:01 -0400

    return throw and break

--- translator/BreakStatement.java
+++ translator/BreakStatement.java
@@ -1,3 +1,24 @@
 /**
  * Identifier?
  */
+
+package translator;
+
+import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Visitor;
+
+public class BreakStatement extends TranslationVisitor { 
+    
+    private String name;
+    
+    public BreakStatement(GNode n)
+    {
+        visit(n);
+    }
+    
+    public void visitIdentifier(Gnode n)
+    {
+        name = n.getString(0);
+    }
+}
