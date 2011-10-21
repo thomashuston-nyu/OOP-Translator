80c9cbb6ff33d08bf3468bb161ac1b518ee5921e
Michael Morreale <michael.j.morreale@gmail.com>
2011-10-21 19:19:21 -0400

    added the Expression and ConditionalExpression classes

--- translator/ConditionalExpression.java
+++ translator/ConditionalExpression.java
@@ -2,3 +2,30 @@
  * (LogicalOrExpression Expression ConditionalExpression)/
  * yyValue:LogicalOrExpression
  */
+package translator;
+
+import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Visitor;
+
+public class ConditionalExpression extends TranslationVisitor {
+  private LogicalOrExpression logicalOrExpression;
+  private Expression expression;
+  private ConditionalExpression conditionalExpression;
+
+  public ConditionalExpression(GNode n) {
+    visit(n);
+  }
+
+  public void visitLogicalOrExpression(GNode n) {
+    logicalOrExpression = new LogicalOrExpression(n);
+  }
+
+  public void visitExpression(GNode n) {
+    expression = new Expression(n);
+  }
+
+  public void visitConditionalExpression(GNode n) {
+    conditionalExpression = new ConditionalExpression(n);
+  }
+}
--- translator/Expression.java
+++ translator/Expression.java
@@ -2,3 +2,30 @@
  * (ConditionalExpression AssignmentOperator Expression)/
  * yyValue:ConditionalExpression
  */
+package translator;
+
+import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Visitor;
+
+public class Expression extends TranslationVisitor {
+  private ConditionalExpression conditionalExpression;
+  private String assignmentOperator;
+  private Expression expression;
+
+  public Expression(GNode n) {
+    visit(n);
+  }
+
+  public void visitConditionalExpression(GNode n) {
+    conditionalExpression = new ConditionalExpression(n);
+  }
+
+  public void visitSymbol(GNode n) {
+    assignmentOperator = n.getString(0);
+  }
+  
+  public void visitExpression(GNode n) {
+    expression = new Expression(n);
+  }
+}
