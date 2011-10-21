3a62cb7052345b7f027869e2939c4d60c6e50bb8
Thomas Huston <tph227@nyu.edu>
2011-10-21 16:50:48 -0400

    Added all those fucking visit methods

--- translator/Statement.java
+++ translator/Statement.java
@@ -1,6 +1,5 @@
 /**
  * Parent of:
- * Block
  
  // Mike
  * ConditionalStatement
@@ -18,6 +17,7 @@
  * BreakStatement
  
  // Thomas
+ * Block
  * ContinueStatement
  * ExpressionStatement
  
@@ -25,3 +25,64 @@
  * AssertStatement              // LOW PRIORITY
  * EmptyStatement               // NOTHING TO DO
  */
+package translator;
+
+import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Visitor;
+
+public class Statement extends TranslationVisitor {
+
+  public Statement(GNode n) {
+    
+  }
+  
+  public void visitBlock(GNode n) {
+    
+  }
+  
+  public void visitBreakStatement(GNode n) {
+    
+  }
+  
+  public void visitConditionalStatement(GNode n) {
+    
+  }
+  
+  public void visitContinueStatement(GNode n) {
+    
+  }
+  
+  public void visitDoWhileStatement(GNode n) {
+    
+  }
+  
+  public void visitExpressionStatement(GNode n) {
+    
+  }
+  
+  public void visitForStatement(GNode n) {
+    
+  }
+  
+  public void visitReturnStatement(GNode n) {
+    
+  }
+  
+  public void visitSwitchStatement(GNode n) {
+    
+  }
+  
+  public void visitThrowStatement(GNode n) {
+    
+  }
+  
+  public void visitTryCatchFinallyStatement(GNode n) {
+    
+  }
+  
+  public void visitWhileStatement(GNode n) {
+    
+  }
+  
+}
