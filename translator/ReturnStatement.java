b7f3b035f250692892ab3672678857ef9295bdf7
marta wilgan <mmw319@nyu.edu>
2011-10-21 16:48:01 -0400

    return throw and break

--- translator/ReturnStatement.java
+++ translator/ReturnStatement.java
@@ -1,3 +1,26 @@
 /**
  * Expression?
  */
+
+package translator;
+
+import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Visitor;
+
+public class ReturnStatement extends TranslationVisitor { 
+    
+    private Expression expression;
+    
+    public ReturnStatement(GNode n)
+    {
+        visit(n);
+    }
+    
+    public void visitExpression(Gnode n)
+    {
+        expression = new Expression(n);
+    }
+    
+
+}
--- translator/ThrowStatement.java
+++ translator/ThrowStatement.java
@@ -1,3 +1,25 @@
 /**
  * Expression
  */
+
+package translator;
+
+import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Visitor;
+
+public class ThrowStatement extends TranslationVisitor { 
+    
+    private Expression expression;
+    
+    public ThrowStatement(GNode n)
+    {
+        visit(n);
+    }
+    
+    public void visitExpression(Gnode n)
+    {
+        expression = new Expression(n);
+    }
+        
+}
