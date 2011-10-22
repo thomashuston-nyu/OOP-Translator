268195db6f7cd56c05c8501c1436709e501e5547
Michael Morreale <michael.j.morreale@gmail.com>
2011-10-21 23:39:06 -0400

    added classes yet again

--- translator/ThisExpression.java
+++ translator/ThisExpression.java
@@ -1,3 +1,20 @@
 /**
  * QualifiedIdentifier?
  */
+package translator;
+
+import xtc.tree.GNode;
+import xtc.tree.Visitor;
+
+public class ThisExpression extends TranslationVisitor {
+  private QualifiedIdentifier qualifiedIdentifier;
+
+  public ThisExpression(GNode n) {
+    qualifiedIdentifier = null;
+    visit(n);
+  }
+  
+  public void visitQualifiedIdentifier(GNode n) {
+    qualifiedIdentifier = new QualifiedIdentifier(n);
+  }
+}
