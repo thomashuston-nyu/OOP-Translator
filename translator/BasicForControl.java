501a1b272de5b55b13f40e259e31706ed6e5eae2
Michael Morreale <michael.j.morreale@gmail.com>
2011-10-21 23:44:26 -0400

    fixed BasicForControl

--- translator/BasicForControl.java
+++ translator/BasicForControl.java
@@ -4,20 +4,24 @@
  */
 package translator;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import xtc.tree.GNode;
 import xtc.tree.Visitor;
 
 public class BasicForControl extends TranslationVisitor {
   private Modifiers modifiers;
   private boolean isFinal;
+  private Type type;
   private Expression expression;
-  private ExpressionList expressionList;
+  private List<ExpressionList> expressionLists;
 
   public BasicForControl(GNode n) {
     modifiers = null;
     isFinal = false;
     expression = null;
-    expressionList = null;
+    expressionLists = new ArrayList<ExpressionList>();
     visit(n);
   }
 
@@ -29,11 +33,15 @@ public class BasicForControl extends TranslationVisitor {
     isFinal = true;
   }
 
+  public void visitType(GNode n) {
+    type = new Type(n);
+  }
+
   public void visitExpression(GNode n) {
     expression = new Expression(n);
   }
 
   public void visitExpressionList(GNode n) {
-    expressionList = new ExpressionList(n);
+    expressionLists.add(new ExpressionList(n));
   }
 }
