b5920dbdcefd4e4a083ae521afd7d6cb829cd020
Nabil Hassein <nabil.hassein@gmail.com>
2011-10-22 14:03:33 -0400

    updated test script

--- test.sh
+++ test.sh
@@ -13,20 +13,20 @@ java_file=${1%.java}
 javac Translator.java
 check_exit "Translator.java doesn't compile"
 
-javac $1
+javac "$1"
 check_exit "the input file doesn't compile"
 
 # TODO: add as many command line args (starting from $2) here as might be passed to the java file
 java "$java_file" > java_output.txt
 check_exit "the input file compiles, but doesn't run correctly"
 
-# TODO: currently, stdout here (i.e. contents of $cpp_file) is not actually pure c++ code. make sure it is, or else modify this script
+# TODO: make sure that generated c++ code doesn't include any extraneous, illegal lines. (for example, see sed command below)
 cpp_file=${1%.java}.cc
 java Translator -translateJava $1 > $cpp_file
 check_exit "the input file compiles and runs, but Translator.java can't accept it"
 
 # remove xtc output that isn't valid c++ code
-sed -i '1,2d' $cpp_file
+sed -i '1,2d' "$cpp_file"
 
 g++ "$cpp_file"
 check_exit "the translated c++ file doesn't compile"
