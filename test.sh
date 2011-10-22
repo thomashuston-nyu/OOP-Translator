5467d5ba1273d0b0bbc2b68f637265d09f6a02ea
Nabil Hassein <nabil.hassein@gmail.com>
2011-10-22 13:27:49 -0400

    changes to testing

--- test.sh
+++ test.sh
@@ -16,14 +16,14 @@ check_exit "Translator.java doesn't compile"
 javac $1
 check_exit "the input file doesn't compile"
 
-# TODO: currently, stdout here (i.e. contents of $cpp_file) is not actually pure c++ code. make sure it is, or else modify this script
-cpp_file=${1%.java}.cc
-java Translator -translateJava $1 > $cpp_file
-check_exit "the input file compiles, but Translator.java can't accept it"
-
 # TODO: add as many command line args (starting from $2) here as might be passed to the java file
 java "$java_file" > java_output.txt
 check_exit "the input file compiles, but doesn't run correctly"
+
+# TODO: currently, stdout here (i.e. contents of $cpp_file) is not actually pure c++ code. make sure it is, or else modify this script
+cpp_file=${1%.java}.cc
+java Translator -translateJava $1 > $cpp_file
+check_exit "the input file compiles and runs, but Translator.java can't accept it"
 
 g++ "$cpp_file"
 check_exit "the translated c++ file doesn't compile"
@@ -33,3 +33,4 @@ check_exit "the translated c++ file compiles, but doesn't run correctly"
 
 diff java_output.txt cpp_output.txt > ${java_file}_errors.txt
 rm ${java_file}.class a.out java_output.txt cpp_output.txt
+echo "diff between java output and c++ output is in ${java_file}_errors.txt"
