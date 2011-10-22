5519492e29da24922201a213ad418634204df4f1
Michael Morreale <michael.j.morreale@gmail.com>
2011-10-21 20:06:08 -0400

    added a lot more expression classes

--- translator/ShiftExpression.java
+++ translator/ShiftExpression.java
@@ -2,3 +2,31 @@
  * (ShiftExpression ShiftOperator AdditiveExpression)/
  * yyValue:AdditiveExpression
  */
+package translator;
+
+import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Visitor;
+
+public class ShiftExpression extends TranslationVisitor {
+  private ShiftExpression shiftExpression;
+  private String shiftOperator;
+  private AdditiveExpression additiveExpression;
+
+  public ShiftExpression(GNode n) {
+    shiftOperator = null;
+    visit(n);
+  }
+
+  public void visitShiftExpression(GNode n) {
+    shiftExpression = new ShiftExpression(n);
+  }
+
+  public void visitSymbol(GNode n) {
+    shiftOperator = n.getString(0);
+  }
+
+  public void visitAdditiveExpression(GNode n) {
+    additiveExpression = new AdditiveExpression(n);
+  }
+}
