995f409e90f91e273b3b4e2440f4d9a435778331
Nabil Hassein <nabil.hassein@gmail.com>
2011-10-21 20:46:38 -0400

    first draft of automated test script

---
+++ test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# check exit status of previous command. exit outputting input message if there was an error
+function check_exit() {
+    if [ "$?" -ne "0" ]; then
+        echo "ERROR:" $1
+        exit
+    fi
+}
+
+javac Translator.java
+check_exit "Translator.java doesn't compile"
+
+javac $1
+check_exit "the input file doesn't compile"
+
+# TODO: currently, stdout here (i.e. contents of $cpp_file) is not actually pure c++ code. make sure it is, or else modify this script
+cpp_file=${1%.java}.cc
+java Translator -translateJava $1 > $cpp_file
+check_exit "the input file compiles, but Translator.java can't accept it"
+
+java_file=${1%.java}
+# TODO: add as many command line args (starting from $2) here as might be passed to the java file
+java "$java_file" > java_output.txt
+check_exit "the input file compiles, but doesn't run correctly"
+
+g++ "$cpp_file"
+check_exit "the translated c++ file doesn't compile"
+
+./a.out > cpp_output.txt
+check_exit "the translated c++ file compiles, but doesn't run correctly"
+
+diff java_output.txt cpp_output.txt > ${java_file}_errors.txt
+rm ${java_file}.class a.out java_output.txt cpp_output.txt
