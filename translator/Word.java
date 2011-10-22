2749e28bf411b70e3e7e7f9e9d55803281a0f834
Michael Morreale <michael.j.morreale@gmail.com>
2011-10-21 23:39:06 -0400

    added classes yet again

--- translator/Word.java
+++ translator/Word.java
@@ -1,3 +1,14 @@
 /**
  * WordCharacters
  */
+package translator;
+
+import xtc.tree.GNode;
+
+public class Word {
+  private String wordCharacters;
+
+  public Word(GNode n) {
+    wordCharacters = n.getString(0);
+  }
+}
