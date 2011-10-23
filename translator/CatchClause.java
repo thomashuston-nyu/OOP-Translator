bcd31e95ee26b04c431b828110c9bbfa14a7a9ee
Michael Morreale <michael.j.morreale@gmail.com>
2011-10-23 18:05:24 -0400

    added CatchClause

--- translator/CatchClause.java
+++ translator/CatchClause.java
@@ -10,9 +10,18 @@ import xtc.tree.GNode;
 import xtc.tree.Visitor;
 
 public class CatchClause extends TranslationVisitor {
-  
+  private FormalParameter formalParameter;
+  private Block block;
+
   public CatchClause(GNode n) {
-    
+    visit(n);
   }
-  
+
+  public void visitFormalParameter(GNode n) {
+    formalParameter = new FormalParameter(n);
+  }
+
+  public void visitBlock(GNode n) {
+    block = new Block(n);
+  }
 }
