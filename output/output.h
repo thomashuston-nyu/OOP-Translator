09e15a1f0b1b5db695f462eca9e361ae7f7c4cf6
Michael Morreale <michael.j.morreale@gmail.com>
2011-10-24 19:45:25 -0400

    added tests for demo

--- output/output.h
+++ output/output.h
@@ -7,31 +7,75 @@
 
 using namespace java::lang;
 
-struct __Test;
-struct __Test_VT;
+struct __Super;
+struct __Super_VT;
 
-typedef __Test* Test;
+typedef __Super* Super;
 
-struct __Test {
-  __Test_VT* __vptr;
+struct __Super {
+  __Super_VT* __vptr;
+  int32_t x;
+
+  __Super(int32_t x1);
+
+  static int32_t getX(Super);
+  static void setX(Super, int32_t);
+  static void printX(Super);
+  static String toString(Super);
 
   static Class __class();
 
-  static __Test_VT __vtable;
+  static __Super_VT __vtable;
 };
 
-struct __Test_VT {
+struct __Super_VT {
   Class __isa;
   int32_t (*hashCode)(Object);
   bool (*equals)(Object, Object);
-  Class (*getClass)(Test);
-  String (*toString)(Object);
+  Class (*getClass)(Super);
+  String (*toString)(Super);
+  void (*printX)(Super);
+  void (*setX)(Super, int32_t);
+  int32_t (*getX)(Super);
 
-  __Test_VT()
-  : __isa(__Test::__class()),
+  __Super_VT()
+  : __isa(__Super::__class()),
   hashCode(&__Object::hashCode),
   equals(&__Object::equals),
-  getClass((Class(*)(Test))&__Object::getClass),
+  getClass((Class(*)(Super))&__Object::getClass),
+  toString(&__Super::toString),
+  printX(&__Super::printX),
+  setX(&__Super::setX),
+  getX(&__Super::getX) {}
+};
+
+
+
+struct __Sample;
+struct __Sample_VT;
+
+typedef __Sample* Sample;
+
+struct __Sample {
+  __Sample_VT* __vptr;
+
+  static Class __class();
+
+  static __Sample_VT __vtable;
+};
+
+struct __Sample_VT {
+  Class __isa;
+  int32_t (*hashCode)(Object);
+  bool (*equals)(Object, Object);
+  Class (*getClass)(Sample);
+  String (*toString)(Object);
+
+  __Sample_VT()
+  : __isa(__Sample::__class()),
+  hashCode(&__Object::hashCode),
+  equals(&__Object::equals),
+  getClass((Class(*)(Sample))&__Object::getClass),
   toString(&__Object::toString) {}
 };
 
---
+++ test/demo/Sub.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Sub extends Super {
+
+  int x;
+
+  public Sub(int x1) {
+    x = x1;
+  }
+
+  public String toString() {
+    return "This is Sub.";
+  }
+
+}
