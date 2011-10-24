5d21b8ed41b379d96efbf8bd35b4b3474840a389
Michael Morreale <michael.j.morreale@gmail.com>
2011-10-24 19:24:09 -0400

    got for loops working

--- output/output.cc
+++ output/output.cc
@@ -1,36 +1,17 @@
 #include "output.h"
 
-__Sample::__Sample(int32_t x1, int32_t y1)
-: __vptr(&__vtable) {
-  x = x1;
-  y = y1;
-}
-
-int32_t __Sample::getX(Sample __this) {
-  return __this->x;
-}
-
-String __Sample::toString(Sample __this) {
-  return __rt::literal("ok");
-}
-
-Class __Sample::__class() {
-  static Class k = new __Class(__rt::literal("Sample"), __Object::__class());
+Class __Test::__class() {
+  static Class k = new __Class(__rt::literal("Test"), __Object::__class());
   return k;
 }
 
-__Sample_VT __Sample::__vtable;
+__Test_VT __Test::__vtable;
 
 int main(void) {
-  Sample sample = new __Sample(3, 8);
-  int32_t a = sample->__vptr->getX(sample);
-  if (a < 2) {
-    println(__rt::literal("a < 2")->data);
-  } else {
-    while (a > 2) {
-      a--;
-    }
-    println(int_to_string(a));
+  int32_t x = 0;
+  for (int32_t i = 0; i < 10; i++) {
+    x++;
+    println(int_to_string(x));
   }
 }
 
