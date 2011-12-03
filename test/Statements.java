0fd41afac1c5fbbb7acebf504c22f84fd3a9424e
Michael Morreale <michael.j.morreale@gmail.com>
2011-12-03 18:27:46 -0500

    updated statement tests

--- test/Statements.java
+++ test/Statements.java
@@ -1,148 +1,132 @@
 public class Statements {
 
   public static void main(String[] args) {
-  
- 
-  testConditionalStatement();
-  
-  //testContinueStatement();
-  
-  testDoWhileStatement();
-  
-  //testExpressionStatement(); *****
-  
-  testForStatement();
-  
-  int i = testReturnStatement();
-  System.out.println("x = " + i);
-  
-  testSwitchStatement();
+    testBreakStatement();
+    testConditionalStatement();
+    testContinueStatement();
+    testDoWhileStatement();
+    testForStatement();
+    testReturnStatement();
+    testSwitchStatement();
+    testThrowStatement();
+    testTryCatchFinallyStatement();
+    testWhileStatement();
+  }
+
+  public static void testBreakStatement() {
+    System.out.println("Break Statements");
     
-  testTryCatchFinallyStatement();
-  
-  testWhileStatement();
+    while (true)
+      break;
+
+    for (int i = 0; i < 5; i++) {
+      if (i == 2)
+        break;
+      System.out.println(i);
+    }
+  }
+
+  public static void testConditionalStatement() {
+    System.out.println("\nConditional Statements");
     
+    int x = 1;
+    int y = 2;
+    
+    if (x == y)
+      System.out.println("x == y");
+    else if (x > y)
+      System.out.println("x > y");
+    else
+      System.out.println("x < y");
   }
-  
-  
-  public static void testConditionalStatement () {
-  
-  	System.out.println("Testing Conditional Statements :");
-  	int x = 1;
-  	int y = 2;
-  	
-  	System.out.println("x = 1 \ny = 2");
-  	
-  	if (x==y) {
-  		System.out.println("x is equal to y");
-  	}
-  	else if (x>y) {
-  		System.out.println("x is greater than y");
-  	}
-  	else {
-  		System.out.println("x is less than y");
-  	}
-  	
-  	if(x != 0) {
-  		; // do nothing
-  		System.out.println("Testing Empty Statement : nothing happens");
-  	}
 
-  }
-  
-  public static void testDoWhileStatement () {
-  
-  	System.out.println("Testing DoWhile Statements :");
-  	
-  	int x = 0;
-  	
-  	System.out.println("x = 0");
-  	
-  	do {
-  		System.out.println("x is equal to zero");
-  		x++; // exit out of loop
-  	} while (x == 0);
-  
-  }
-  
-  
-  public static void testExpressionStatement () {
+  public static void testContinueStatement() {
+    System.out.println("\nContinue Statements");
 
+    for (int i = 0; i < 5; i++) {
+      if (i % 2 == 0)
+        continue;
+      System.out.println(i);
+    }
   }
-  
-  public static void testForStatement () {
-  
-  	System.out.println("Testing For Statements : ");
-  	System.out.println("i will count from 0 to 9");
-  	
-  	for(int i = 0; i <10; i++) {
-  		System.out.println("i is now " + i);
-  	}
-  }
-  
-  public static int testReturnStatement () {
-  
-  	System.out.println("Testing Return Statement : ");
-  
-  	int x = 0;
-  	System.out.println("x is equal to 0");
-  	return x;
 
+  public static void testDoWhileStatement() {
+    System.out.println("\nDo/While Statements");
+    
+    int x = 0;
+    do {
+      System.out.println(x);
+      x++;
+    } while (x < 3);
   }
-  
-  public static void testSwitchStatement () {
-  
-  	System.out.println("Testing Swicth, Break, and Default Statements : ");
-  
-  	int x = 0;
-  	
-  	switch(x) {
-  		case 1: System.out.println("x is equal to 1"); break;
-  		case 2: System.out.println("x is equal to 2"); break;
-  		case 3: System.out.println("x is equal to 3"); break;
-  		default: System.out.println("default case"); break;
-  	}
 
+  public static void testForStatement() {
+    System.out.println("\nFor Statements");
+
+    for (int i = 0; i < 3; i++)
+      System.out.println(i);
   }
-  
-  
-  public static void testTryCatchFinallyStatement () {
-  
-  	System.out.println("Testing Try Catch Finally and Throws Statements : ");  	
-  	
-  	int [] x = {0,1,2};
-  	System.out.println("Array x is of length 3");
-  	
-  	try
-  	{
-  		System.out.println("Trying to access array x at index 3 which is out of bounds");
-  		int y = x[3];
-  		throw new ArrayIndexOutOfBoundsException();
-  	}
-  	catch (ArrayIndexOutOfBoundsException e)
-  	{
-  		System.out.println("Array Index Out of Bounds Exception is caught!");
-  	}
-  	finally
-  	{
-  		System.out.println("Executing finally block");
-  	}  	
 
+  public static void testReturnStatement() {
+    System.out.println("\nReturn Statements");
+
+    System.out.println(returnHelper());
   }
-  
-  public static void testWhileStatement () {
-  
-  System.out.println("Testing While Statements :");
-  	
-  	int x = 0;
-  	
-  	System.out.println("x = 0");
-  	
-  	while (x == 0) {
-  		System.out.println("x is equal to zero");
-  		x++;// get out of loop
-  	}
 
+  public static int returnHelper() {
+    return 3;
+  }
+
+  public static void testSwitchStatement() {
+    System.out.println("\nSwitch Statements");
+
+    int x = 2;
+    switch (x) {
+      case 1:
+        System.out.println("case 1");
+        break;
+      case 2:
+        System.out.println("case 2");
+      case 3:
+        System.out.println("case 3");
+        break;
+      default:
+        System.out.println("default");
+        break;
+    }
+  }
+
+  public static void testThrowStatement() {
+    System.out.println("\nThrow Statements");
+
+    try {
+      throw new NullPointerException();
+    } catch (NullPointerException e) {
+      System.out.println("Exception caught");
+    }
+  }
+
+  public static void testTryCatchFinallyStatement() {
+    System.out.println("\nTry/Catch/Finally Statements");
+
+    int[] x = {1, 2, 3, 4, 5};
+    try {
+      int y = x[5];
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Exception caught");
+    } finally {
+      System.out.println("Finally executed");
+    }
+  }
+
+  public static void testWhileStatement() {
+    System.out.println("\nWhile Statements");
+
+    int x = 0;
+    while (x < 3) {
+      System.out.println(x);
+      x++;
+    }
   }
 
 }
